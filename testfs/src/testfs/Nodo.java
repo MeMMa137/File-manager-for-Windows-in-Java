8898d8255fbeca168b066bcbef15d9b5209a2f9d
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-16 11:15:01 +0200

    Created Node class, with errors

---
+++ testfs/src/testfs/Nodo.java
@@ -0,0 +1,36 @@
+package testfs;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.LinkedList;
+
+public class Nodo {
+
+    String nome;
+    Color fileColor;
+    LinkedList<Nodo> figli;
+    LinkedList<String> files;
+    int x, y;
+
+    public Nodo(String path) {
+        fileColor = Color.blue;
+        x = 0;
+        y = 0;
+        String[] p = path.split("\\\\");
+        nome = p[p.length - 1];
+        figli = new LinkedList<>();
+        files = new LinkedList<>();
+        try {
+            File f = new File(path);
+            for (File sub : f.listFiles()) {
+                String name = sub.getName();
+                if (sub.isDirectory()) {
+                    Nodo nn = new Nodo(path + "\\" + name);
+                    figli.add(nn);
+                } else {
+                    files.add(name);
+                }
+            }
+        }
+    }
+}
